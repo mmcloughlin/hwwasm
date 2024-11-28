d5b08ede2f6942b0f11594e6f7a0d2176c9c6974
Michael McLoughlin <mcloughlin@cmu.edu>
2024-11-27 19:03:27 -0500

    example: implement vsha1cq_u32

--- example/sha1/wasm_arm_neon.c
+++ example/sha1/wasm_arm_neon.c
@@ -32,9 +32,35 @@ uint32x4_t vreinterpretq_u32_u8(uint8x16_t a) { return a; }
 
 uint8x16_t vreinterpretq_u8_u32(uint32x4_t a) { return a; }
 
+#define SHA1_CHOOSE(X, Y, Z) (((Y ^ Z) & X) ^ Z)
+
+#define SHA1_ROUND(F, I) do { \
+        uint32_t f = F(b, c, d); \
+        uint32_t t = __builtin_rotateleft32(a, 5) + f + e + wasm_u32x4_extract_lane(wk, I); \
+        e = d; \
+        d = c; \
+        c = __builtin_rotateleft32(b, 30); \
+        b = a; \
+        a = t; \
+    } while(0)
+
+uint32x4_t vsha1cq_u32(uint32x4_t hash_abcd, uint32_t hash_e, uint32x4_t wk) {
+    uint32_t a = wasm_u32x4_extract_lane(hash_abcd, 0);
+    uint32_t b = wasm_u32x4_extract_lane(hash_abcd, 1);
+    uint32_t c = wasm_u32x4_extract_lane(hash_abcd, 2);
+    uint32_t d = wasm_u32x4_extract_lane(hash_abcd, 3);
+    uint32_t e = hash_e;
+
+    SHA1_ROUND(SHA1_CHOOSE, 0);
+    SHA1_ROUND(SHA1_CHOOSE, 1);
+    SHA1_ROUND(SHA1_CHOOSE, 2);
+    SHA1_ROUND(SHA1_CHOOSE, 3);
+
+    return wasm_u32x4_make(a, b, c, d);
+}
+
 // TODO: uint32x4_t vld1q_u32(uint32_t const * ptr);
 // TODO: void vst1q_u32(uint32_t * ptr, uint32x4_t val);
-// TODO: uint32x4_t vsha1cq_u32(uint32x4_t hash_abcd, uint32_t hash_e, uint32x4_t wk);
 // TODO: uint32x4_t vsha1pq_u32(uint32x4_t hash_abcd, uint32_t hash_e, uint32x4_t wk);
 // TODO: uint32x4_t vsha1mq_u32(uint32x4_t hash_abcd, uint32_t hash_e, uint32x4_t wk);
 // TODO: uint32_t vsha1h_u32(uint32_t hash_e);
