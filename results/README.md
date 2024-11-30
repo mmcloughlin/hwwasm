f0f50121a2b009a49fc0c9dd53a52b0aee29d679
Michael McLoughlin <mcloughlin@cmu.edu>
2024-11-29 21:22:22 -0500

    results: summary

---
+++ results/README.md
@@ -0,0 +1,10 @@
+| Commit | Change | vs. Native |
+| --- | --- | --- |
+| https://github.com/mmcloughlin/hwwasmtime/commit/0ba331eeafa1 | `sha1c` | x7.64 |
+| https://github.com/mmcloughlin/hwwasmtime/commit/e637ffd61aa7 | `sha1pm` | x5.11 |
+| https://github.com/mmcloughlin/hwwasmtime/commit/20ee379e7e52 | `sha1h` | x5.03 |
+| https://github.com/mmcloughlin/hwwasmtime/commit/6e842bbf9754 | `sha1su0` | x5.12 |
+| https://github.com/mmcloughlin/hwwasmtime/commit/a388e067b323 | `sha1su1` | x3.59 |
+| https://github.com/mmcloughlin/hwwasmtime/commit/0ebda4d1c4ee | `vaddq_u32` | x3.76 |
+| https://github.com/mmcloughlin/hwwasmtime/commit/779aa6937dc8 | `vdupq_n_u32` | x2.71 |
+| https://github.com/mmcloughlin/hwwasmtime/commit/7f5d2e9d5928 | `vgetq_lane_u32` | x3.10 |
---
+++ tools/results.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import collections
+from glob import glob
+
+
+class Result(collections.namedtuple("Result", ["metadata", "runs"])):
+    @property
+    def name(self):
+        return self.metadata["name"]
+
+    def is_wip(self):
+        return self.name == "wip"
+
+    def native_run(self):
+        return self.runs["native"]
+
+    def validate(self):
+        # Disallow WIP results
+        assert not self.is_wip()
+
+        # Git versions should not be dirty
+        assert not self.metadata["hwwasm_git_version"].endswith("-dirty")
+        assert not self.metadata["wasmtime_hwwasm_git_version"].endswith("-dirty")
+
+        # Wasmtime version should be forked from baseline.
+        assert self.metadata["wasmtime_hwwasm_version"].startswith(self.metadata["wasmtime_baseline_version"])
+
+        # Wasmtime fork should be built from the git version hash.
+        git_version = self.metadata["wasmtime_hwwasm_git_version"][:9]
+        assert git_version in self.metadata["wasmtime_hwwasm_version"]
+
+        # All runs should agree on parameters and final state.
+        baseline = self.native_run()
+        for run in self.runs.values():
+            assert run["message_blocks"] == baseline["message_blocks"]
+            assert run["iterations"] == baseline["iterations"]
+            assert run["final_state"] == baseline["final_state"]
+
+
+def read_result(path):
+    data_files = glob("*.json", root_dir=path)
+
+    # Metadata
+    METADATA_FILE = "metadata.json"
+    data_files.remove(METADATA_FILE)
+    with open(os.path.join(path, METADATA_FILE)) as f:
+        metadata = json.load(f)
+
+    # Run results
+    runs = {}
+    for run_file in data_files:
+        name, _ = os.path.splitext(run_file)
+        with open(os.path.join(path, run_file)) as f:
+            run = json.load(f)
+            runs[name] = run
+
+    return Result(metadata, runs)
+
+
+def read_results(root_dir):
+    results = []
+    for name in os.listdir(root_dir):
+        path = os.path.join(root_dir, name)
+        if os.path.isdir(path):
+            result = read_result(path)
+            if not result.is_wip():
+                results.append(result)
+    results.sort(key=lambda r: r.metadata["timestamp"])
+    return results
+
+
+def report(results):
+    print(f"| Commit | Change | vs. Native |")
+    print(f"| --- | --- | --- |")
+    for result in results:
+        name = result.name
+        native_run = result.native_run()
+        intrinsics_run = result.runs["wasmtime_hwwasm"]
+        scale = intrinsics_run["elapsed_ns"] / native_run["elapsed_ns"]
+        commit_url = "https://github.com/mmcloughlin/hwwasmtime/commit/" + result.metadata["wasmtime_hwwasm_git_version"]
+        print(f"| {commit_url} | `{name}` | x{scale:.2f} |")
+
+
+def main():
+    results = read_results("results")
+    for result in results:
+        result.validate()
+    report(results)
+
+
+if __name__ == "__main__":
+    main()
